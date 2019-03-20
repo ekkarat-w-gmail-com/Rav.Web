0adcdc1f517221419706cd8ce6b2f1bc9f3aefbc
Patrick Edqvist <patrick.edqvist@gmail.com>
2019-03-20 18:02:08 +0100

    Added klarna confirmation

--- src/pages/thank-you.js
+++ src/pages/thank-you.js
@@ -1,14 +1,45 @@
 // @flow
-import React from 'react'
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { get } from 'lodash/fp';
 
+// Actions
+import { getKlarnaCheckoutById } from '../store/actions';
+
+// Components
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import { KlarnaCheckout } from '../components/KlarnaCheckout';
 
-const ThankYou = () => (
-  <Layout>
-    <SEO title={'Thanks for your purchase!'} />
-    <h1>Thanks for your purchase!</h1>
-  </Layout>
-)
+type Props = {
+ location: Object,
+ getKlarnaCheckoutById: (orderId: string) => void
+}
 
-export default ThankYou
+const ThankYou = (props: Props) => {
+
+  const urlParams = new URLSearchParams(props.location.search);
+  const myParam = urlParams.get('sid');
+
+  useEffect(() => {
+    if ( myParam ) {
+      props.getKlarnaCheckoutById(myParam);
+    }
+  }, [ myParam ])
+
+  return (
+
+    <Layout>
+      <SEO title={'Thanks for your purchase!'} />      
+      <KlarnaCheckout html={get('checkout.html_snippet', props)} />
+    </Layout>
+
+  );
+}
+
+const mapStateToProps = (store) => ({
+  checkout: store.checkout
+})
+
+
+export default connect(mapStateToProps, { getKlarnaCheckoutById })(ThankYou)
